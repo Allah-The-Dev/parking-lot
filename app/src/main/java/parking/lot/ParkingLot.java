9eb63f652c0f2d49a3f1f15948c92cd220d3d4cc
Neelesh kumar pandey <neelesh.pandey@idfcbank.com>
2023-12-13 12:13:29 +0530

    Neelesh/Hitesh : throws CarAlreadyParkedException if user tries to park again

--- app/src/main/java/parking/lot/ParkingLot.java
+++ app/src/main/java/parking/lot/ParkingLot.java
@@ -1,16 +1,24 @@
 package parking.lot;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ParkingLot {
     int capacity;
+    Set<Car> parkedCars = new HashSet<>();
 
     public ParkingLot(int capacity) {
         this.capacity = capacity;
     }
 
-    public void park(Car car) throws ParkingLotFullException {
+    public void park(Car car) throws ParkingLotFullException, CarAlreadyParkedException {
         if (capacity <= 0) {
             throw new ParkingLotFullException();
         }
+        if (parkedCars.contains(car)) {
+            throw new CarAlreadyParkedException();
+        }
+        parkedCars.add(car);
         capacity--;
     }
 
--- app/src/test/java/parking/lot/ParkingLotTest.java
+++ app/src/test/java/parking/lot/ParkingLotTest.java
@@ -1,20 +1,20 @@
 package parking.lot;
 
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class ParkingLotTest {
     @Test
-    void shouldBeAbleToParkCar() throws ParkingLotFullException {
+    void shouldBeAbleToParkCar() throws ParkingLotFullException, CarAlreadyParkedException {
         ParkingLot parkingLot = new ParkingLot(1000);
 
         parkingLot.park(new Car());
     }
 
     @Test
-    void shouldThrowParkingNotAvailableExceptionForNoCapacity() throws ParkingLotFullException {
+    void shouldThrowParkingNotAvailableExceptionForNoCapacity()
+            throws ParkingLotFullException, CarAlreadyParkedException {
         ParkingLot parkingLot = new ParkingLot(3);
 
         parkingLot.park(new Car());
@@ -22,6 +22,21 @@ public class ParkingLotTest {
         parkingLot.park(new Car());
 
         assertThrows(ParkingLotFullException.class, () -> parkingLot.park(new Car()));
+    }
+
+    @Test
+    void shouldThrowCarAlreadyParkedExceptionForParkingSameCarAgain()
+            throws ParkingLotFullException, CarAlreadyParkedException {
+        ParkingLot parkingLot = new ParkingLot(4);
+
+        parkingLot.park(new Car());
+        parkingLot.park(new Car());
+
+        Car car3 = new Car();
+        parkingLot.park(car3);
+
+        assertThrows(CarAlreadyParkedException.class, () -> parkingLot.park(car3));
+
     }
 
 }
