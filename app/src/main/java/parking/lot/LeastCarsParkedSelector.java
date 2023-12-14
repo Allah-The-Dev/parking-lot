5a8025c00d330510368febf51e60cb3fe621e302
Neelesh kumar pandey <pandeyneelkumar@gmail.com>
2023-12-14 11:44:20 +0530

    Neelesh/Hitesh : replaces if else with java stream API to select least parked and most capacity one

--- app/src/main/java/parking/lot/LeastCarsParkedSelector.java
+++ app/src/main/java/parking/lot/LeastCarsParkedSelector.java
@@ -1,25 +1,17 @@
 package parking.lot;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class LeastCarsParkedSelector implements ParkingLotSelector {
 
     @Override
     public ParkingLot select(List<ParkingLot> parkingLots) {
-        ParkingLot parkingLotMinimum = new ParkingLot(0);
-        if (parkingLots.size() > 0) {
-            parkingLotMinimum = parkingLots.get(0);
-        }
-
-        for (int i = 1; i < parkingLots.size(); i++) {
-            ParkingLot parkingLot = parkingLots.get(i);
-            if (parkingLotMinimum.getParkedCarsCount() > parkingLot.getParkedCarsCount()) {
-                if (!parkingLot.isFull()) {
-                    parkingLotMinimum = parkingLot;
-                }
-            }
-        }
-        return parkingLotMinimum;
+        return parkingLots
+                .stream()
+                .filter(parkingLot -> !parkingLot.isFull())
+                .min(Comparator.comparing(parkingLot -> parkingLot.getParkedCarsCount()))
+                .orElse(new ParkingLot(0));
     }
 
 }
--- app/src/main/java/parking/lot/MostCapacityParkingLotSelector.java
+++ app/src/main/java/parking/lot/MostCapacityParkingLotSelector.java
@@ -1,22 +1,17 @@
 package parking.lot;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class MostCapacityParkingLotSelector implements ParkingLotSelector {
 
     @Override
     public ParkingLot select(List<ParkingLot> parkingLots) {
-        ParkingLot maxCapacityParkingLot = new ParkingLot(0);
-        if (parkingLots.size() > 0) {
-            maxCapacityParkingLot = parkingLots.get(0);
-        }
-        for (int i = 1; i < parkingLots.size(); i++) {
-            ParkingLot parkingLot = parkingLots.get(i);
-            if (parkingLot.getCapacity() > maxCapacityParkingLot.getCapacity() && !parkingLot.isFull()) {
-                maxCapacityParkingLot = parkingLot;
-            }
-        }
-        return maxCapacityParkingLot;
+        return parkingLots
+                .stream()
+                .filter(parkingLot -> !parkingLot.isFull())
+                .max(Comparator.comparing(parkingLot -> parkingLot.getCapacity()))
+                .orElse(new ParkingLot(0));
     }
 
 }
